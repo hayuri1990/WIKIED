738d2cc65c8e56345602c6bb7ee0e8fe836f8963
hayuri1990 <hayuri1990@gmail.com>
2025-09-21 14:05:10 +0900

    feat: 로딩 스피너 컴포넌트 및 스타일 추가

---
+++ components/common/loadingSpinner/index.tsx
@@ -0,0 +1,20 @@
+import styles from '@/components/common/loadingSpinner/styles.module.scss';
+import { DotPulse } from '@uiball/loaders';
+
+interface LoadingSpinnerProps {
+  size?: number;
+  speed?: number;
+  color?: string;
+}
+
+export const LoadingSpinner = ({
+  size = 43,
+  speed = 1.3,
+  color = '#4cbfa4',
+}: LoadingSpinnerProps) => {
+  return (
+    <div className={styles['loading-spinner-background']}>
+      <DotPulse size={size} speed={speed} color={color} />
+    </div>
+  );
+};
